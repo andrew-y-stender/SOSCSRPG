fc04de040213637fbcbf680962f40ebd643cee09
Andrew Stender <stender_yoonchul@yahoo.com>
2020-11-17 01:36:04 -0800

    added monster class and monster factory and images for three monsters

---
+++ Engine/Factories/MonsterFactory.cs
@@ -0,0 +1,56 @@
+﻿using Engine.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Engine.Factories
+{
+    public static class MonsterFactory
+    {
+        public static Monster GetMonster(int monsterID)
+        {
+            switch (monsterID)
+            {
+                case 1:
+                    Monster snake =
+                        new Monster("Snake", "snake.png", 4, 4, 5, 1);
+
+                    AddLootItem(snake, 9001, 25);
+                    AddLootItem(snake, 9002, 75);
+
+                    return snake;
+
+                case 2:
+                    Monster rat =
+                       new Monster("Rat", "rat.jpeg", 5, 5, 5, 1);
+
+                    AddLootItem(rat, 9003, 25);
+                    AddLootItem(rat, 9004, 75);
+
+                    return rat;
+
+                case 3:
+                    Monster spider =
+                        new Monster("Spider", "spider.jpeg", 10, 10, 10, 3);
+
+                    AddLootItem(spider, 9005, 25);
+                    AddLootItem(spider, 9006, 75);
+
+                    return spider;
+
+                default:
+                    throw new ArgumentException(string.Format("MonsterType '{0}' does not exist", monsterID));
+            }
+        }
+
+        private static void AddLootItem(Monster monster, int itemID, int percentage)
+        {
+            if (RandomNumberGenerator.NumberBetween(1, 100) <= percentage)
+            {
+                monster.Inventory.Add(new ItemQuantity(itemID, 1));
+            }
+        }
+    }
+}
