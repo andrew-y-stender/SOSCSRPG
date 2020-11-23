fb5d8d0eeabf7913281a31a716d7de839cd9b453
Andrew Stender <stender_yoonchul@yahoo.com>
2020-11-23 02:26:30 -0800

    light re-factoring

--- Engine/Factories/WorldFactory.cs
+++ Engine/Factories/WorldFactory.cs
@@ -15,45 +15,45 @@ namespace Engine.Factories
 
             newWorld.AddLocation(0, -1, "Home",
                 "You currently reside in Culver City",
-                "/Engine;component/Images/Locations/CulverCityRevise.jpg");
+                "CulverCityRevise.jpg");
 
             newWorld.AddLocation(0, 0, "Beverly Hills",
                 "You have arrived in Beverly Hills",
-                "/Engine;component/Images/Locations/BeverlyHillsRevise.jpg");
+                "BeverlyHillsRevise.jpg");
 
             newWorld.AddLocation(0, 1, "Christian Louboutin",
                 "You have arrived at Christian Louboutin",
-                "/Engine;component/Images/Locations/LouboutinRevise.jpg");
+                "LouboutinRevise.jpg");
 
             newWorld.LocationAt(0, 1).QuestsAvailableHere.Add(QuestFactory.GetQuestByID(1));
 
             newWorld.AddLocation(0, 2, "West Hollywood",
                 "You currently arrived in Weho",
-                "/Engine;component/Images/Locations/WehoRevise.jpg");
+                "WehoRevise.jpg");
 
             newWorld.LocationAt(0, 2).AddMonster(1, 100);
 
             newWorld.AddLocation(1, 0, "10 Freeway",
                 "You are currently *stuck* on the 10 Freeway",
-                "/Engine;component/Images/Locations/FreewayRevise.jpg");
+                "FreewayRevise.jpg");
 
             newWorld.AddLocation(2, 0, "Skid Row",
                 "You are arrived at Skid Row",
-                "/Engine;component/Images/Locations/SkidRowRevise.jpg");
+                "SkidRowRevise.jpg");
 
             newWorld.LocationAt(2, 0).AddMonster(3, 100);
 
             newWorld.AddLocation(-1, 0, "UCLA",
                 "You are arrived at UCLA",
-                "/Engine;component/Images/Locations/UCLARevise.jpg");
+                "UCLARevise.jpg");
 
             newWorld.AddLocation(-1, -1, "3rd St. Promenade",
                 "You are arrived at the 3rd St. Promenade",
-                "/Engine;component/Images/Locations/3rdStreetRevise.jpg");
+                "3rdStreetRevise.jpg");
 
             newWorld.AddLocation(-2, -1, "Venice Beach",
                 "You are arrived at Venice Beach",
-                "/Engine;component/Images/Locations/VeniceBeachRevise.jpg");
+                "VeniceBeachRevise.jpg");
 
             newWorld.LocationAt(-2, -1).AddMonster(2, 100);
 
--- Engine/ViewModels/GameSession.cs
+++ Engine/ViewModels/GameSession.cs
@@ -57,40 +57,20 @@ namespace Engine.ViewModels
                 }
             }
         }
-
+        
         public Weapon CurrentWeapon { get; set; }
 
-        public bool HasLocationToNorth
-        {
-            get
-            {
-                return CurrentWorld.LocationAt(CurrentLocation.XCooridnate, CurrentLocation.YCoordinate + 1) != null;
-            }
-        }
+        public bool HasLocationToNorth =>
+            CurrentWorld.LocationAt(CurrentLocation.XCooridnate, CurrentLocation.YCoordinate + 1) != null;
 
-        public bool HasLocationToWest
-        {
-            get
-            {
-                return CurrentWorld.LocationAt(CurrentLocation.XCooridnate - 1, CurrentLocation.YCoordinate) != null;
-            }
-        }
+        public bool HasLocationToEast =>
+            CurrentWorld.LocationAt(CurrentLocation.XCooridnate + 1, CurrentLocation.YCoordinate) != null;
 
-        public bool HasLocationToEast
-        {
-            get
-            {
-                return CurrentWorld.LocationAt(CurrentLocation.XCooridnate + 1, CurrentLocation.YCoordinate) != null;
-            }
-        }
+        public bool HasLocationToSouth =>
+            CurrentWorld.LocationAt(CurrentLocation.XCooridnate, CurrentLocation.YCoordinate - 1) != null;
 
-        public bool HasLocationToSouth
-        {
-            get
-            {
-                return CurrentWorld.LocationAt(CurrentLocation.XCooridnate, CurrentLocation.YCoordinate - 1) != null;
-            }
-        }
+        public bool HasLocationToWest =>
+            CurrentWorld.LocationAt(CurrentLocation.XCooridnate - 1, CurrentLocation.YCoordinate) != null;
 
         public bool HasMonster => CurrentMonster != null;
 
